bdfde8495ba420b01d8d2eb9d99e48c0f9d4bf39
Arthur Borges <arthur@debian>
2022-05-20 13:40:04 -0300

    Adicionando o menu lateral Eu tentei de várias formas abrir e fechar o menu lateral apenas usando CSS, mas não obtive êxito. Então adicionei duas funções javascript para exibir ou ocultar o menu lateral no evento de clique dos respectivos botões.

---
+++ script.js
@@ -0,0 +1,7 @@
+function menuShow (){
+    document.getElementById('nav-menu').style.display = 'flex';
+}
+
+function menuHide (){
+    document.getElementById('nav-menu').style.display = 'none';
+}
